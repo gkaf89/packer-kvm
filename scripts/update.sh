e4d25e0d296494b15b9370f2214f14d70ca06b81
Georgios Kafanas <g.kafanas@greeklug.gr>
2023-11-01 22:21:52 +0100

    Use long format flags in scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,3 @@
 # Update the box
-DEBIAN_FRONTEND=noninteractive apt -y update
-DEBIAN_FRONTEND=noninteractive apt -y full-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get --yes update
+DEBIAN_FRONTEND=noninteractive apt-get --yes full-upgrade
